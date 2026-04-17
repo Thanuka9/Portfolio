1819009e29591386070c8091466818d4089987d4
Thanuka <Thanuka.ellepola@gmail.com>
2026-04-17 21:40:52 +0530

    fix: authorize drive.google.com for image fetching

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.postimg.cc',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
     ],
   },
 };
